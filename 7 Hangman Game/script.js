158ab9607aaffba9f3d5eef4635a27f6052245fe
Tina.Lin <7070587@gmail.com>
2020-11-03 17:45:11 +0800

    7 Hangman Game js  | update keydown and show show notification

--- 7 Hangman Game/script.js
+++ 7 Hangman Game/script.js
@@ -12,11 +12,12 @@ const words = ["application", "programming", "interface", "wizard", "procrss", "
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ["j", "u", "i", "c", "e"];
+const correctLetters = [];
 const wrongLetters = [];
 
 // show hidden word
 function displayWord() {
+  // array.includes(value): check this value weather in array or not
   wordEle.innerHTML = `
     ${selectedWord
       .split("")
@@ -40,3 +41,44 @@ function displayWord() {
 }
 
 displayWord();
+
+// show notification
+function showNotification() {
+  notification.classList.add("show");
+  setTimeout(() => notification.classList.remove("show"), timeout);
+}
+
+// update wrong letters elememt
+function updateWrongLettersElememt() {
+  console.log("updateWrongLettersElememt => ");
+}
+
+// keydown letter press
+window.addEventListener("keydown", (e) => {
+  //   console.log(" => ", e.key);
+  const regex = /[a-z]/i;
+  if (regex.test(e.key)) {
+    console.log(" => ", 123);
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      // push correct letter in correctLetters, if this letter is not in correctLetters
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        // this correct letter is already in correctLetters, then show this letter
+        showNotification();
+      }
+    } else {
+      // this not correct push to wrongLetters
+      // push not correct letter in wrongLetters, if this letter is not in wrongLetters
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLettersElememt();
+      } else {
+        // this correct letter is already in correctLetters, then show this letter
+        showNotification();
+      }
+    }
+  }
+});
