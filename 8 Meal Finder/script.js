bc94e6616d761fad8ddc8f0d4e0f975dc8217f99
Tina.Lin <7070587@gmail.com>
2020-11-04 13:40:13 +0800

    8 Meal Finder /  script.js  | update  show Single Meal

--- 8 Meal Finder/script.js
+++ 8 Meal Finder/script.js
@@ -20,7 +20,6 @@ async function searchMeal(e) {
   if (searchValue.trim()) {
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`);
     const data = await res.json();
-    // console.log("data => ", data);
 
     // search value title
     resultHeadingEle.innerHTML = `<h2>Search results for '${searchValue}':</h2>`;
@@ -29,14 +28,13 @@ async function searchMeal(e) {
     if (data.meals === null) {
       resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
     } else {
-      console.log(" => ", data.meals);
       mealsEle.innerHTML = data.meals
         .map(
           (meal) =>
             `
             <div class="meal">
               <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="meal__img"/>
-              <div class="meal__info" data-mealID="${meal.idMeal}">
+              <div class="meal__info" data-mealId="${meal.idMeal}">
                 <h3>${meal.strMeal}</h3>
               </div>
             </div>
@@ -50,3 +48,53 @@ async function searchMeal(e) {
 }
 
 submit.addEventListener("submit", searchMeal);
+
+// fetch meal by Id
+async function getMealById(mealId) {
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+  const data = await res.json();
+  const meal = data.meals[0];
+
+  addMealToDOM(meal);
+}
+
+// add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+  for (let index = 1; index < 30; index++) {
+    if (!meal[`strIngredient${index}`]) break;
+    ingredients.push(`${meal[`strIngredient${index}`]} - ${meal[`strMeasure${index}`]}`);
+  }
+
+  singleMealEle.innerHTML = `
+    <div class="single__meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single__meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
+// show detail meal
+mealsEle.addEventListener("click", (e) => {
+  //   console.log(" => ", e.path);
+  const mealInfo = e.path.find((item) => {
+    if (!item.classList) return false;
+    return item.classList.contains("meal__info");
+  });
+
+  if (mealInfo) {
+    const mealId = mealInfo.getAttribute("data-mealId");
+    getMealById(mealId);
+  }
+});
