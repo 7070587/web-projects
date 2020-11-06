8239d11e2cd8d1cca1915cb1c66d98dfe2de745f
Tina.Lin <7070587@gmail.com>
2020-11-06 15:38:35 +0800

    11 Infinite Scroll Posts script.js | update get posts data from api

---
+++ 11 Infinite Scroll Posts/script.js
@@ -0,0 +1,35 @@
+const postsContainer = document.getElementById("post__container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 5;
+let page = 1;
+
+// fetch from api
+async function getPosts() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+  const data = await res.json();
+  return data;
+}
+
+// show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+  posts.forEach((post) => {
+    const postELe = document.createElement("div");
+    postELe.classList.add("post");
+    postELe.innerHTML = `
+            <div class="post__number">${post.id}</div>
+            <div class="post__info">
+                 <h2 class="post__title">${post.title}</h2>
+                 <div class="post__body">${post.body}</div>
+            </div>
+    `;
+
+    postsContainer.appendChild(postELe);
+  });
+}
+
+let aaa =
+  // init show posts
+  showPosts();
