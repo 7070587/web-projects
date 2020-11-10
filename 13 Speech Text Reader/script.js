c143240ec9af8382eaf1114efd89be7b4e816349
Tina.Lin <7070587@gmail.com>
2020-11-10 11:02:26 +0800

    13 Speech Text Reader index.html + script.js  | update html and box element use js to show

---
+++ 13 Speech Text Reader/script.js
@@ -0,0 +1,77 @@
+const main = document.querySelector("main");
+
+const textBox = document.getElementById("text__box");
+const selectVoices = document.getElementById("text__box-voices");
+const textarea = document.getElementById("text__box-textarea");
+
+const btnToggle = document.getElementById("btn-toggle");
+const btnRead = document.getElementById("btn-read");
+const btnClose = document.getElementById("text__box-close");
+
+const data = [
+  {
+    image: "./images/drink.jpg",
+    text: "I'm Thirsty",
+  },
+  {
+    image: "./images/food.jpg",
+    text: "I'm Hungry",
+  },
+  {
+    image: "./images/tired.jpg",
+    text: "I'm Tired",
+  },
+  {
+    image: "./images/hurt.jpg",
+    text: "I'm Hurt",
+  },
+  {
+    image: "./images/happy.jpg",
+    text: "I'm Happy",
+  },
+  {
+    image: "./images/angry.jpg",
+    text: "I'm Angry",
+  },
+  {
+    image: "./images/sad.jpg",
+    text: "I'm Sad",
+  },
+  {
+    image: "./images/scared.jpg",
+    text: "I'm Scared",
+  },
+  {
+    image: "./images/outside.jpg",
+    text: "I Want To Go Outside",
+  },
+  {
+    image: "./images/home.jpg",
+    text: "I Want To Go Home",
+  },
+  {
+    image: "./images/school.jpg",
+    text: "I Want To Go To School",
+  },
+  {
+    image: "./images/grandma.jpg",
+    text: "I Want To Go To Grandmas",
+  },
+];
+
+data.forEach(createBox);
+
+// create speech box
+function createBox(item) {
+  const box = document.createElement("div");
+  const { image, text } = item;
+  box.classList.add("box");
+  box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+  // todo speak event
+
+  main.appendChild(box);
+}
