6bdc60bf12f8984157c142892cb7df808770330e
Tina.Lin <7070587@gmail.com>
2020-11-13 10:04:43 +0800

    15 Lyrics Search App script.js| update show song in DOM and next / prev page

--- 15 Lyrics Search App/script.js
+++ 15 Lyrics Search App/script.js
@@ -20,11 +20,48 @@ async function searchSongs(searchValue) {
   const res = await fetch(`${apiURL}/suggest/${searchValue}`);
   const data = await res.json();
 
-  showData(data);
+  console.log(" => ", data);
+
+  if (data) showData(data);
 }
 
 // show song and artist in DOM
-function showData(data) {}
+function showData(dataValue) {
+  result.innerHTML = `
+        <ul class='songs'>
+            ${dataValue.data
+              .map(
+                (song) =>
+                  `
+                  <li>
+                    <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                    <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+                  </li>
+                `
+              )
+              .join("")}
+        </ul>
+    `;
+
+  // check if have prev or next
+  if (dataValue.prev || dataValue.next) {
+    more.innerHTML = `
+      ${dataValue.prev ? `<button class='btn' onclick="getMoreSongs('${dataValue.prev}')">Prev</button>` : ""}
+      ${dataValue.next ? `<button class='btn' onclick="getMoreSongs('${dataValue.next}')">Next</button>` : ""}
+        `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+// get next or prev songs
+async function getMoreSongs(url) {
+  // resolve cross problem https://cors-anywhere.herokuapp.com
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  if (data) showData(data);
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
