dfa20f3a3c31d70ebb4dac4e005a3e53706b7c94
Tina.Lin <7070587@gmail.com>
2020-11-17 10:07:24 +0800

    18 New Year Countdown script.js| update year spinner

--- 18 New Year Countdown/script.js
+++ 18 New Year Countdown/script.js
@@ -1,5 +1,6 @@
 const year = document.getElementById("year");
 const countdown = document.getElementById("countdown");
+const loading = document.getElementById("loading");
 
 const days = document.getElementById("days");
 const hours = document.getElementById("hours");
@@ -8,6 +9,15 @@ const seconds = document.getElementById("seconds");
 
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`Jan 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
+
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
 
 function updateCountdown() {
   const currentTime = new Date();
@@ -18,10 +28,12 @@ function updateCountdown() {
   const minute = Math.floor(diff / 1000 / 60) % 60;
   const second = Math.floor(diff / 1000) % 60;
 
+  // add value to DOM
   days.innerHTML = day;
   hours.innerHTML = hour.toString().padStart(2, "0");
   minutes.innerHTML = minute.toString().padStart(2, "0");
   seconds.innerHTML = second.toString().padStart(2, "0");
 }
 
+// Run every second
 setInterval(updateCountdown, 1000);
