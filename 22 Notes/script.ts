9adc9f9346ca573138dd5d9894368e492d59328d
Tina.Lin <7070587@gmail.com>
2020-11-19 17:23:04 +0800

    22 Notes script.ts  | update add note

--- 22 Notes/script.ts
+++ 22 Notes/script.ts
@@ -1,17 +1,43 @@
-const btnEdit: HTMLElement = document.getElementById("btn-edit");
-const btnDelete: HTMLElement = document.getElementById("btn-delete");
+const btnAdd: HTMLElement = document.getElementById("btn-add");
 
 const notesEle: HTMLElement = document.getElementById("notes");
 
-const main: HTMLElement = document.querySelector(".notes__main");
-const textarea: HTMLElement = document.querySelector(".notes__main-textarea");
+btnAdd.addEventListener("click", () => addNewNote());
 
-btnEdit.addEventListener("click", () => {
-  main.classList.toggle("hidden");
-  textarea.classList.toggle("hidden");
-});
+function addNewNote() {
+  const note = document.createElement("div");
+  note.classList.add("notes");
 
-textarea.addEventListener("input", (e: any) => {
-  const { value } = e.target;
-  main.innerHTML = marked(value);
-});
+  note.innerHTML = `
+        <div class="notes__tools">
+            <button class="fas fa-edit btn" id='btn-edit'></button>
+            <button class="fas fa-trash-alt btn" id='btn-delete'></button>
+        </div>
+
+        <div class="notes__main hidden"> </div>
+        <textarea class="notes__main-textarea"></textarea>
+
+    `;
+
+  const btnEdit: HTMLElement = note.querySelector(".fa-edit");
+  const btnDelete: HTMLElement = note.querySelector(".fa-trash-alt");
+
+  const main: HTMLElement = note.querySelector(".notes__main");
+  const textarea: HTMLElement = note.querySelector(".notes__main-textarea");
+
+  btnEdit.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textarea.classList.toggle("hidden");
+  });
+
+  btnDelete.addEventListener("click", () => {
+    note.remove();
+  });
+
+  textarea.addEventListener("input", (e: any) => {
+    const { value } = e.target;
+    main.innerHTML = marked(value);
+  });
+
+  document.body.appendChild(note);
+}
