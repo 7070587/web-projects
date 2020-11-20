dabaf1bd4d0b9c11b3da7a9fd2a42218f9684bfb
Tina.Lin <7070587@gmail.com>
2020-11-20 11:28:44 +0800

    23 Todo script.ts  | update save data in localStorage

--- 23 Todo/script.ts
+++ 23 Todo/script.ts
@@ -1,26 +1,63 @@
+interface ITodo {
+  text: string;
+  completed: boolean;
+}
+
 const form: HTMLElement = document.getElementById("form");
 const input: HTMLInputElement = <HTMLInputElement>document.getElementById("input");
 const todos: HTMLElement = document.getElementById("todos");
 
+const todoData: ITodo[] = JSON.parse(localStorage.getItem("todos"));
+
+// add localStorage to DOM
+function addLocalStorageToDOM() {
+  (todoData || []).forEach((todo) => addTodo(todo));
+}
+
+addLocalStorageToDOM();
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  addTodo();
+});
 
-  const todoText = input.value;
+// input to add todo
+function addTodo(todo: ITodo = undefined) {
+  const todoText = todo && todo.text ? todo.text : input.value;
 
   if (todoText) {
     const todoEle: HTMLElement = document.createElement("li");
+
+    if (todo && todo.completed) {
+      todoEle.classList.add("completed");
+    }
+
     todoEle.innerHTML = todoText;
 
-    todoEle.addEventListener("click", () => todoEle.classList.toggle("completed"));
+    todoEle.addEventListener("click", () => {
+      todoEle.classList.toggle("completed");
+      updateTodos();
+    });
 
     // click mouse right to dele todo
-    todoEle.addEventListener("contextmenu", () => {
+    todoEle.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEle.remove();
+      updateTodos();
     });
 
     todos.appendChild(todoEle);
 
     input.value = "";
+
+    updateTodos();
   }
-});
+}
+
+// save todo localStorage
+function updateTodos() {
+  const todosEle: NodeList = document.querySelectorAll("li");
+  const todos: ITodo[] = [];
+  todosEle.forEach((todo: HTMLElement) => todos.push({ text: todo.innerText, completed: todo.classList.contains("completed") }));
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
