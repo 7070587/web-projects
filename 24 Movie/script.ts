5bad8aec3f7f7690256cf99c778317a1bd3fab53
Tina.Lin <7070587@gmail.com>
2020-11-23 15:18:57 +0800

    24 Movie index.html + srtyle.css + script.ts | update search

--- 24 Movie/script.ts
+++ 24 Movie/script.ts
@@ -3,16 +3,23 @@ const IMGPATH: string = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI: string = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main: HTMLElement = document.getElementById("main");
+const form: HTMLElement = document.getElementById("form");
+const search: HTMLInputElement = <HTMLInputElement>document.getElementById("search");
 
 // get movies
-async function getMovies(): Promise<any> {
-  const res = await fetch(APIURL);
+async function getMovies(url: string): Promise<any> {
+  const res = await fetch(url);
   const data = await res.json();
 
-  console.log(" => ", data);
+  //   console.log(" => ", data);
+  showMovieToDOM(data.results);
+  return data;
+}
 
-  // get movie image
-  (data.results || []).forEach((movie) => {
+function showMovieToDOM(movie: any[]) {
+  // clear main movie data
+  main.innerHTML = "";
+  (movie || []).forEach((movie) => {
     const { poster_path, title, vote_average } = movie;
     const movieEle: HTMLElement = document.createElement("div");
     movieEle.classList.add("movie");
@@ -26,8 +33,6 @@ async function getMovies(): Promise<any> {
       `;
     main.appendChild(movieEle);
   });
-  HTMLElement;
-  return data;
 }
 
 function getClassByScore(vote_average: number): string {
@@ -40,4 +45,14 @@ function getClassByScore(vote_average: number): string {
   }
 }
 
-getMovies();
+getMovies(APIURL);
+
+// search
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchValue: string = search.value.trim();
+
+  const url: string = searchValue ? SEARCHAPI + searchValue : APIURL;
+
+  getMovies(url);
+});
