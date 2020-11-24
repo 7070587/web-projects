22e029f83e9684531510e69b25179270ee8f52bf
Tina.Lin <7070587@gmail.com>
2020-11-24 14:17:49 +0800

    26 Drawing index.html + script.ts | update canvas color and size

--- 26 Drawing/script.ts
+++ 26 Drawing/script.ts
@@ -1,7 +1,13 @@
 const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const btnIncrease: HTMLElement = document.getElementById("increase");
+const btnDecrease: HTMLElement = document.getElementById("decrease");
+const canvasSize: HTMLInputElement = <HTMLInputElement>document.getElementById("canvasSize");
+const canvasColor: HTMLInputElement = <HTMLInputElement>document.getElementById("color");
+
 let size: number = 5;
+let color: string = "black";
 let x: number = 10;
 let y: number = 10;
 
@@ -22,8 +28,31 @@ canvas.addEventListener("mousemove", (e) => {
 function drawCircle(x: number, y: number): void {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
   ctx.fill();
 }
+
+btnIncrease.addEventListener("click", () => {
+  size++;
+  if (size > 50) size = 50;
+  canvasSize.value = `${size}`;
+});
+
+btnDecrease.addEventListener("click", () => {
+  size--;
+  if (size < 1) size = 1;
+  canvasSize.value = `${size}`;
+});
+
+canvasSize.addEventListener("input", () => {
+  if (+canvasSize.value > 50) canvasSize.value = "50";
+  if (+canvasSize.value < 1) canvasSize.value = "1";
+});
+
+canvasColor.addEventListener("change", (e: any) => {
+  color = e.target.value;
+  console.log(" => ", color);
+});
 
 // drawCircle(100, 100);
 
